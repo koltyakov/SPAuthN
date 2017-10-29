9795a152c350a31b312ec472557796f29cabfd54
Andrew Koltyakov <koltyakov@gmail.com>
2017-10-30 00:37:27 +0300

    Usage example

--- SPAuthN.Test/Program.cs
+++ SPAuthN.Test/Program.cs
@@ -1,19 +1,69 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
 
 namespace SPAuthN.Test
 {
-
     class Program
     {
         static void Main(string[] args)
         {
 
-            Headers authHeaders = SPAuth.GetAuth("--configPath='./config/private.json'");
-            Console.WriteLine(authHeaders.Cookie);
+            Options options = SPAuth.GetAuth("--configPath='./config/private.json'");
+            Console.WriteLine("SharePoint web URL: {0}", options.SiteUrl);
+            foreach (var key in options.Headers.AllKeys)
+            {
+                Console.WriteLine("\nHeader: {0} \nValue: {1}", key, options.Headers[key]);
+            }
+            Console.WriteLine("");
 
-            var bp = "bp"; bp = "";
+            WebRequest request = WebRequest.Create(options.SiteUrl + "/_api/web?$select=Title");
+            request.Headers = options.Headers;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.NoContent)
+            {
+                Stream dataStream = response.GetResponseStream();
+                XDocument xDoc = XDocument.Load(dataStream);
+
+                XNamespace ns = "http://www.w3.org/2005/Atom";
+                XNamespace d = "http://schemas.microsoft.com/ado/2007/08/dataservices";
+                XNamespace m = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata";
+
+                string title = xDoc
+                    .Element(ns + "entry").Element(ns + "content")
+                    .Element(m + "properties").Element(d + "Title").Value;
+
+                Console.WriteLine("REST | Web title is: {0}", title);
+
+                dataStream.Close();
+            }
+
+            response.Close();
+
+            using (ClientContext clientContext = new ClientContext(options.SiteUrl))
+            {
+                clientContext.ExecutingWebRequest += (sender, arguments) =>
+                {
+                    foreach (var key in options.Headers.AllKeys)
+                    {
+                        arguments.WebRequestExecutor.RequestHeaders[key] = options.Headers[key];
+                    }
+                };
+
+                var web = clientContext.Web;
+                clientContext.Load(web);
+                clientContext.ExecuteQuery();
+
+                Console.WriteLine("CSOM | Web title is: {0}", web.Title);
+            }
+
+
+            var bp = "";
 
         }
     }
-
 }
